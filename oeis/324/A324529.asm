094aa470c7cb68fc6d86465bc5bc319bd5e640ab
Simon Strandgaard <neoneye@gmail.com>
2022-06-22 00:20:22 +0200

    Updated programs

--- oeis/324/A324529.asm
+++ oeis/324/A324529.asm
@@ -3,7 +3,9 @@
 ; 1,6,12,56,30,36,56,960,351,900,132,12096,182,1176,1800,31744,306,75816,380,168000,14112,4356,552,1658880,3875,14196,29160,21952,870,810000,992,2064384,17424,31212,58800,917070336,1406,21660,85176,23040000,1722,6223392
 
 mov $1,$0
-seq $1,274536 ; a(n) = 6 * sigma(n).
-seq $0,244668 ; Numerators of (product of divisors of n / sum of divisors of n).
-mul $0,$1
-div $0,6
+seq $1,203 ; a(n) = sigma(n), the sum of the divisors of n. Also called sigma_1(n).
+mov $2,$1
+seq $0,324502 ; a(n) = denominator of  Sum_{d|n} (1/pod(d)) where pod(k) = the product of the divisors of k (A007955).
+gcd $1,$0
+div $2,$1
+mul $0,$2
