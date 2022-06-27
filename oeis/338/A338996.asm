dbf94ca90186961c3c9739e6e6596a524b1c3d76
Simon Strandgaard <neoneye@gmail.com>
2022-06-27 17:07:03 +0200

    Updated programs

--- oeis/338/A338996.asm
+++ oeis/338/A338996.asm
@@ -4,15 +4,12 @@
 
 add $0,1
 mov $4,$0
-mul $0,2
 lpb $0
-  sub $0,2
-  add $4,1
-  sub $5,$2
-  sub $2,1
-  add $3,2
-  mul $3,$5
+  sub $0,1
+  add $3,$0
   add $1,$3
   mov $3,$4
+  add $5,$1
+  add $2,$5
 lpe
-mov $0,$1
+mov $0,$2
