5fd0eddb0fad93d970974118ed960dcc0402472b
Simon Strandgaard <neoneye@gmail.com>
2022-07-20 16:42:58 +0200

    Updated programs

--- oeis/053/A053616.asm
+++ oeis/053/A053616.asm
@@ -1,12 +1,7 @@
 ; A053616: Pyramidal sequence: distance to nearest triangular number.
+; Submitted by Simon Strandgaard
 ; 0,0,1,0,1,1,0,1,2,1,0,1,2,2,1,0,1,2,3,2,1,0,1,2,3,3,2,1,0,1,2,3,4,3,2,1,0,1,2,3,4,4,3,2,1,0,1,2,3,4,5,4,3,2,1,0,1,2,3,4,5,5,4,3,2,1,0,1,2,3,4,5,6,5,4,3,2,1,0,1,2,3,4,5,6,6,5,4,3,2,1,0,1,2,3,4,5,6,7,6
 
-lpb $0,5
-  trn $3,$1
-  add $3,$0
-  add $5,1
-  trn $0,$5
-  mov $1,$3
-  mov $3,$5
-lpe
-mov $0,$1
+mul $0,2
+seq $0,4739 ; Concatenation of sequences (1,2,2,...,n-1,n-1,n,n,n-1,n-1,...,2,2,1) for n >= 1.
+div $0,2
--- oeis/053/A053645.asm
+++ oeis/053/A053645.asm
@@ -1,11 +1,9 @@
 ; A053645: Distance to largest power of 2 less than or equal to n; write n in binary, change the first digit to zero, and convert back to decimal.
+; Submitted by Simon Strandgaard
 ; 0,0,1,0,1,2,3,0,1,2,3,4,5,6,7,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36
 
+mov $1,1
 lpb $0
-  sub $0,1
-  mov $1,$0
-  mul $2,2
-  add $2,1
-  trn $0,$2
+  sub $0,$1
+  mul $1,2
 lpe
-mov $0,$1
