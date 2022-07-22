e3870587e6ab321ae27e10a036575541c7686edc
Simon Strandgaard <neoneye@gmail.com>
2022-07-23 00:37:56 +0200

    Updated programs

--- oeis/006/A006672.asm
+++ oeis/006/A006672.asm
@@ -1,10 +1,8 @@
 ; A006672: Ramsey numbers.
+; Submitted by Simon Strandgaard
 ; 4,4,6,7,8,9,11,12,13,14
 
-mul $0,3
-div $0,-2
-mul $0,4
-div $0,5
-sub $1,$0
-add $1,4
-mov $0,$1
+mul $0,5
+sub $0,2
+div $0,4
+add $0,4
