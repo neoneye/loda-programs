d93221e47e1ee714df099b3c80ecf9dc4f3eef34
Simon Strandgaard <neoneye@gmail.com>
2022-07-22 23:29:24 +0200

    Updated programs

--- oeis/276/A276868.asm
+++ oeis/276/A276868.asm
@@ -1,8 +1,19 @@
 ; A276868: First differences of the Beatty sequence A276855 for 3 + tau, where tau = golden ratio = (1 + sqrt(5))/2.
-; Submitted by http://kodeks.karelia.ru/
+; Submitted by Simon Strandgaard
 ; 4,5,4,5,5,4,5,4,5,5,4,5,5,4,5,4,5,5,4,5,4,5,5,4,5,5,4,5,4,5,5,4,5,5,4,5,4,5,5,4,5,4,5,5,4,5,5,4,5,4,5,5,4,5,4,5,5,4,5,5,4,5,4,5,5,4,5,5,4,5,4,5,5,4,5,4,5,5,4,5,5,4,5,4,5,5,4,5,5,4,5,4,5,5,4,5,4,5,5,4
 
-seq $0,130526 ; A permutation of the integers induced by the lower and upper Wythoff sequences.
-min $0,1
-max $0,0
-add $0,4
+mov $4,$0
+mov $3,2
+lpb $3
+  div $3,2
+  mov $0,$4
+  add $0,$3
+  seq $0,60143 ; a(n) = floor(n/tau), where tau = (1 + sqrt(5))/2.
+  mov $2,$3
+  mul $2,$0
+  mov $5,$0
+  add $1,$2
+  add $1,2
+lpe
+sub $1,$5
+mov $0,$1
