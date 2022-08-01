c2dcea9072526400380df21c66a9f2aca86685b4
Simon Strandgaard <neoneye@gmail.com>
2022-08-01 22:20:24 +0200

    Updated programs

--- oeis/161/A161840.asm
+++ oeis/161/A161840.asm
@@ -1,19 +1,7 @@
 ; A161840: Number of noncentral divisors of n.
-; Submitted by SystemViper
+; Submitted by Simon Strandgaard
 ; 0,0,0,2,0,2,0,2,2,2,0,4,0,2,2,4,0,4,0,4,2,2,0,6,2,2,2,4,0,6,0,4,2,2,2,8,0,2,2,6,0,6,0,4,4,2,0,8,2,4,2,4,0,6,2,6,2,2,0,10,0,2,4,6,2,6,0,4,2,6,0,10,0,2,4,4,2,6,0,8,4,2,0,10,2,2,2,6,0,10,2,4,2,2,2,10,0,4,4,8
 
-add $0,1
-mov $2,$0
-lpb $2
-  add $2,1
-  add $3,2
-  sub $4,1
-  mov $5,$0
-  mod $5,$4
-  cmp $5,0
-  add $1,$5
-  sub $2,$3
-lpe
-mov $0,$1
+seq $0,38548 ; Number of divisors of n that are at most sqrt(n).
 sub $0,1
 mul $0,2
