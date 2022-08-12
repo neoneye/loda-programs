7bc19bc664116b662be0d8e73d477f59981b18f4
Simon Strandgaard <neoneye@gmail.com>
2022-08-12 13:51:09 +0200

    Updated programs

--- oeis/091/A091704.asm
+++ oeis/091/A091704.asm
@@ -1,26 +1,20 @@
 ; A091704: Number of Barker codes (or Barker sequences) of length n up to reversals and negations.
-; Submitted by Christian Krause
+; Submitted by Simon Strandgaard
 ; 2,1,2,1,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
 mov $2,2
-mov $5,1
+add $0,3
 lpb $0
-  mov $3,$0
+  mov $3,2
   lpb $3
-    add $0,1
-    mul $2,$5
-    mov $1,$2
-    cmp $1,0
-    add $2,$1
-    mov $4,$3
-    mod $4,$2
+    sub $0,3
+    mov $1,$0
+    mod $1,$2
     add $2,1
-    div $3,2
-    cmp $4,0
-    cmp $4,0
-    mov $5,$4
+    sub $3,$1
   lpe
   div $0,$2
-  mov $2,$5
+  pow $0,2
+  mov $2,1
 lpe
-mov $0,$2
+mov $0,$3
