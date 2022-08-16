e860eec92612b86aabfc718e0ce319c3a236cea3
Simon Strandgaard <neoneye@gmail.com>
2022-08-16 23:03:58 +0200

    Updated programs

--- oeis/213/A213771.asm
+++ oeis/213/A213771.asm
@@ -6,16 +6,18 @@ lpb $0
   add $2,1
   sub $0,$2
 lpe
+sub $3,$0
+mul $3,2
+mov $4,$0
+mul $4,3
+add $4,1
 add $2,1
 sub $2,$0
-mov $4,$0
 mov $0,$2
 lpb $0
   sub $0,1
-  add $1,$3
-  add $1,$3
-  add $4,1
   add $3,$4
   add $1,$3
+  add $4,3
 lpe
 mov $0,$1
