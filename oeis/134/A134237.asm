46f704662327a6907c57c8ca73c765cff7562876
Simon Strandgaard <neoneye@gmail.com>
2022-08-19 22:53:35 +0200

    Updated programs

--- oeis/134/A134237.asm
+++ oeis/134/A134237.asm
@@ -1,15 +1,13 @@
 ; A134237: Triangle read by rows, a(1) = 1, n-th row n terms of: (2n-1, 2n, 2n+1, ..., followed by n).
-; Submitted by Christian Krause
+; Submitted by Simon Strandgaard
 ; 1,3,2,5,6,3,7,8,9,4,9,10,11,12,5,11,12,13,14,15,6,13,14,15,16,17,18,7,15,16,17,18,19,20,21,8,17,18,19,20,21,22,23,24,9,19,20,21,22,23,24,25,26,27,10,21,22,23,24,25,26,27,28,29,30,11,23,24,25,26,27,28,29,30,31,32,33,12
 
-mov $2,1
+mov $1,1
 lpb $0
-  add $2,1
-  sub $0,$2
+  add $1,1
+  sub $0,$1
 lpe
-mov $1,$0
-lpb $1
-  sub $1,$0
-  mul $2,2
-lpe
-add $0,$2
+bin $2,$0
+gcd $2,2
+mul $1,$2
+add $0,$1
--- oeis/292/A292930.asm
+++ oeis/292/A292930.asm
@@ -8,13 +8,12 @@ lpb $0
 lpe
 mov $1,$0
 add $1,3
-bin $1,$0
-add $2,1
-mov $3,$1
+bin $1,3
+sub $2,$0
 add $0,1
 lpb $0
   sub $0,1
-  mul $3,$2
-  sub $2,1
+  add $2,1
+  mul $1,$2
 lpe
-mov $0,$3
+mov $0,$1
