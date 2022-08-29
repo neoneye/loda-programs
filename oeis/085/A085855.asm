adad74717efbe344b5cbe55a5898e1a5224a406f
Simon Strandgaard <neoneye@gmail.com>
2022-08-29 21:13:15 +0200

    Updated programs

--- oeis/085/A085855.asm
+++ oeis/085/A085855.asm
@@ -1,6 +1,13 @@
 ; A085855: Number of 1's in decimal expansion of Fibonacci(n).
-; Submitted by zombie67 [MM]
+; Submitted by Simon Strandgaard
 ; 0,1,1,0,0,0,0,1,1,0,0,0,1,0,0,1,0,1,0,2,0,1,3,0,0,0,2,2,3,1,0,1,1,0,0,0,1,2,1,0,2,3,1,0,1,3,3,2,0,0,1,2,1,3,1,1,2,1,1,1,1,2,1,1,2,3,0,1,2,2,2,4,2,1,1,3,1,0,1,2,1,1,5,1,2,3,2,2,5,4,3,1,2,5,3,1,1,2,2,2
 
 seq $0,45 ; Fibonacci numbers: F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
-seq $0,268643 ; Number of 1's in decimal representation of n.
+lpb $0
+  mov $2,$0
+  mod $2,10
+  cmp $2,1
+  div $0,10
+  add $1,$2
+lpe
+mov $0,$1
--- oeis/242/A242448.asm
+++ oeis/242/A242448.asm
@@ -1,18 +1,9 @@
 ; A242448: Number of distinct linear polynomials b+c*x in row n of array generated as in Comments.
-; Submitted by Jon Maiga
+; Submitted by Simon Strandgaard
 ; 1,3,6,12,22,38,64,106,174,284,462,750,1216,1970,3190,5164,8358,13526,21888,35418,57310,92732,150046,242782,392832,635618
 
-mov $1,1
-lpb $0
-  sub $0,1
-  sub $4,2
-  sub $3,$4
-  mov $4,0
-  div $1,2
-  mul $1,2
-  add $1,1
-  mov $2,$3
-  mov $3,$1
-  add $1,$2
-lpe
-mov $0,$1
+seq $0,78642 ; Numbers with two representations as the sum of two Fibonacci numbers.
+mov $1,6
+div $1,$0
+add $0,$1
+sub $0,4
