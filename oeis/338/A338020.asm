fca0e1394666a240b1d0f0f46ed7c144342fef4f
Simon Strandgaard <neoneye@gmail.com>
2022-09-03 21:56:21 +0200

    Updated programs

--- oeis/338/A338020.asm
+++ oeis/338/A338020.asm
@@ -1,20 +1,24 @@
 ; A338020: a(n) is the number of circles of positive integer area with radii less than n and greater than n - 1.
-; Submitted by Jamie Morken(w4)
+; Submitted by Simon Strandgaard
 ; 3,9,16,22,28,35,40,48,53,60,66,72,78,85,91,98,103,110,117,122,129,135,141,148,154,160,167,173,179,185,192,197,205,210,217,223,229,236,242,248,255,260,267,274,279,286,292,299,304,311,318,323,330,336,343,349,355,361,367
 
-mov $2,$0
-mov $4,2
-lpb $4
-  sub $4,1
-  mov $0,$2
-  add $0,$4
-  max $0,0
-  seq $0,66643 ; a(n) = floor(Pi*n^2).
-  mov $1,$4
-  mul $1,$0
-  add $3,$1
+mov $5,$0
+mov $3,2
+lpb $3
+  sub $3,1
+  mov $0,$5
+  add $0,$3
+  pow $0,2
+  seq $0,22844 ; a(n) = floor(n*Pi).
+  mov $4,$0
+  mov $2,$3
+  lpb $2
+    sub $2,1
+    mov $1,$0
+  lpe
 lpe
-min $2,1
-mul $2,$0
-mov $0,$3
-sub $0,$2
+lpb $5
+  mov $5,0
+  sub $1,$4
+lpe
+mov $0,$1
