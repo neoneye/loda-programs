5efba31e34a7d16236efb162f0a6dfd27c3f6060
Simon Strandgaard <neoneye@gmail.com>
2022-09-04 14:30:54 +0200

    Updated programs

--- oeis/033/A033966.asm
+++ oeis/033/A033966.asm
@@ -1,23 +1,20 @@
 ; A033966: Trajectory of 1 under map n->19n+1 if n odd, n->n/2 if n even
-; Submitted by Christian Krause
+; Submitted by Simon Strandgaard
 ; 1,20,10,5,96,48,24,12,6,3,58,29,552,276,138,69,1312,656,328,164,82,41,780,390,195,3706,1853,35208,17604,8802,4401,83620,41810,20905,397196,198598,99299,1886682,943341,17923480
 
+add $0,1
 mov $1,$0
-mov $0,3
+min $0,1
 lpb $1
   mov $2,$0
+  mov $3,$0
+  dif $0,2
   mod $2,2
-  mov $3,1
-  sub $3,$2
   lpb $2
     sub $2,1
     mul $0,19
-    add $0,3
-  lpe
-  lpb $3
-    sub $3,1
-    div $0,2
+    add $0,1
   lpe
   sub $1,1
 lpe
-div $0,3
+mov $0,$3
--- oeis/297/A297351.asm
+++ oeis/297/A297351.asm
@@ -1,7 +1,8 @@
 ; A297351: Smallest number k such that, for any set S of k distinct nonzero residues mod p = prime(n), any residue mod p can be represented as a sum of zero or more distinct elements of S.
+; Submitted by Simon Strandgaard
 ; 1,2,3,4,6,6,7,8,9,10,10,11,12,12,13,14,15,15,16,16,16,17
 
-seq $0,40 ; The prime numbers.
-sub $0,2
-seq $0,307136 ; a(n) = ceiling(2*sqrt(A000037(n))), n >= 1.
-sub $0,2
+mul $0,4
+seq $0,219954 ; (A160414(n)-1)/4, n >= 1.
+add $0,1
+seq $0,48766 ; Integer part of cube root of n. Or, number of cubes <= n. Or, n appears 3n^2 + 3n + 1 times.
