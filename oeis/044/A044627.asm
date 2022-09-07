591e78057cf65e43bc5720372cdf5abe8e0ccbc2
Simon Strandgaard <neoneye@gmail.com>
2022-09-07 19:30:26 +0200

    Updated programs

--- oeis/044/A044627.asm
+++ oeis/044/A044627.asm
@@ -2,5 +2,8 @@
 ; Submitted by Simon Strandgaard
 ; 59,123,187,251,315,379,443,479,507,571,635,699,763,827,891,955,991,1019,1083,1147,1211,1275,1339,1403,1467,1503,1531,1595,1659,1723,1787,1851,1915,1979,2015,2043,2107,2171,2235,2299,2363
 
-seq $0,44625 ; Numbers n such that string 7,1 occurs in the base 8 representation of n but not of n+1.
-seq $0,86799 ; Replace all trailing 0's with 1's in binary representation of n.
+seq $0,44248 ; Numbers n such that string 7,5 occurs in the base 8 representation of n but not of n-1.
+mov $1,8
+gcd $1,$0
+add $1,1
+sub $0,$1
--- oeis/321/A321029.asm
+++ oeis/321/A321029.asm
@@ -7,6 +7,7 @@ mov $2,2
 add $0,1
 lpb $0
   mov $3,$0
+  sub $3,1
   lpb $3
     mov $4,$0
     mod $4,$2
@@ -19,8 +20,7 @@ lpb $0
   lpb $0
     dif $0,$2
     mul $5,$2
-    sub $5,$4
-    max $5,0
+    trn $5,$4
     add $5,1
     mov $4,1
   lpe
