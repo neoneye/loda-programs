68da8669141651bd624b4ab748793d022f33e1dc
Simon Strandgaard <neoneye@gmail.com>
2022-09-09 14:00:45 +0200

    Updated programs

--- oeis/271/A271491.asm
+++ oeis/271/A271491.asm
@@ -1,35 +1,16 @@
 ; A271491: Arises in enumeration of locally convex functions.
-; Submitted by pelpolaris
+; Submitted by Simon Strandgaard
 ; 1,3,9,16,20,21,21,21,21,21,21,21
 
-mov $6,2
-mov $8,$0
-mov $4,$0
-lpb $4
-  sub $4,1
-  mov $7,0
-  mov $0,$8
-  sub $0,$4
-  mov $1,2
-  mov $2,1
-  mov $3,$0
-  mul $3,5
-  lpb $3
-    add $7,1
-    mul $2,$3
-    add $1,$2
-    add $2,$1
-    mov $5,$7
-    div $5,3
-    mov $9,$5
-    cmp $9,0
-    add $5,$9
-    div $1,$5
-    div $2,$5
-    div $2,8
-    sub $3,1
-  lpe
-  add $6,$2
+lpb $0
+  sub $0,1
+  mov $2,4
+  bin $2,$0
+  mov $3,2
+  bin $3,$1
+  add $3,$2
+  add $1,1
+  add $4,$3
 lpe
-mov $0,$6
-sub $0,1
+mov $0,$4
+add $0,1
--- oeis/289/A289426.asm
+++ oeis/289/A289426.asm
@@ -1,43 +1,15 @@
 ; A289426: a(n) = length of longest circuit code K(n,5).
-; Submitted by Jamie Morken(s1)
+; Submitted by Simon Strandgaard
 ; 0,4,6,8,10,12,14,16,24,28,40
 
-mov $9,$0
-mov $4,$0
-lpb $4
-  sub $4,1
-  mov $7,0
-  mov $0,$9
-  sub $0,$4
-  mov $1,2
-  mov $2,1
-  mov $3,$0
-  mul $3,5
-  lpb $3
-    add $7,1
-    mul $2,$3
-    mov $5,$7
-    div $5,3
-    mov $10,$5
-    cmp $10,0
-    add $5,$10
-    add $1,$2
-    div $1,$5
-    add $1,1
-    add $5,10
-    div $2,$5
-    sub $3,1
-  lpe
-  mov $6,5
-  pow $6,$0
-  div $2,$6
-  mov $10,$2
-  cmp $10,0
-  add $2,$10
-  div $1,$2
-  mov $0,$1
-  mod $0,10
-  mul $0,2
-  add $8,$0
+mov $2,$0
+lpb $0
+  sub $0,6
+  add $1,1
+  add $1,$2
+  mov $2,$0
+  trn $0,1
+  mul $0,3
 lpe
-mov $0,$8
+mul $1,2
+mov $0,$1
