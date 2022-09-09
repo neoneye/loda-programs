688c50fd71498f0ac3022146d5f36649070e8198
Simon Strandgaard <neoneye@gmail.com>
2022-09-09 19:56:57 +0200

    Updated programs

--- oeis/128/A128971.asm
+++ oeis/128/A128971.asm
@@ -6,12 +6,11 @@ lpb $0
   add $1,1
   sub $0,$1
 lpe
+sub $1,$0
 add $1,2
-mov $2,$1
-sub $2,$0
-mov $3,2
-pow $3,$0
+mov $2,2
+pow $2,$0
 mov $0,2
-pow $0,$2
+pow $0,$1
 div $0,3
-mul $0,$3
+mul $0,$2
