68899154774191c52e3159085f19fa8f3ab17950
Simon Strandgaard <neoneye@gmail.com>
2022-09-16 01:40:10 +0200

    Updated programs

--- oeis/078/A078705.asm
+++ oeis/078/A078705.asm
@@ -1,5 +1,13 @@
 ; A078705: Integer part of the square root of sigma(n).
+; Submitted by Simon Strandgaard
 ; 1,1,2,2,2,3,2,3,3,4,3,5,3,4,4,5,4,6,4,6,5,6,4,7,5,6,6,7,5,8,5,7,6,7,6,9,6,7,7,9,6,9,6,9,8,8,6,11,7,9,8,9,7,10,8,10,8,9,7,12,7,9,10,11,9,12,8,11,9,12,8,13,8,10,11,11,9,12,8,13,11,11,9,14,10,11,10,13,9,15,10,12,11,12,10,15,9,13,12,14
 
+mov $1,3
 seq $0,203 ; a(n) = sigma(n), the sum of the divisors of n. Also called sigma_1(n).
-seq $0,196 ; Integer part of square root of n. Or, number of positive squares <= n. Or, n appears 2n+1 times.
+sub $0,1
+lpb $0
+  sub $0,$1
+  add $1,2
+lpe
+div $1,2
+mov $0,$1
--- oeis/325/A325321.asm
+++ oeis/325/A325321.asm
@@ -1,8 +1,6 @@
 ; A325321: a(n) = 1 if cototient of n is a square, 0 otherwise.
-; Submitted by nenym
+; Submitted by Simon Strandgaard
 ; 1,1,1,0,1,1,1,1,0,0,1,0,1,0,0,0,1,0,1,0,1,0,1,1,0,0,1,1,1,0,1,1,0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,1,1,1,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0
 
-seq $0,16035 ; a(n) = Sum_{j|n, 1 < j < n} phi(j). Also a(n) = n - phi(n) - 1 for n > 1.
-seq $0,49240 ; Smallest nonnegative value taken on by x^2 - n*y^2 for an infinite number of integer pairs (x, y).
-add $0,1
-mod $0,2
+seq $0,51953 ; Cototient(n) := n - phi(n).
+seq $0,10052 ; Characteristic function of squares: a(n) = 1 if n is a square, otherwise 0.
