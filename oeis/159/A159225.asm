b8644ca9a5f72382d1bb184bcfdb61c38571ceec
Simon Strandgaard <neoneye@gmail.com>
2022-09-19 15:33:12 +0200

    Updated programs

--- oeis/159/A159225.asm
+++ oeis/159/A159225.asm
@@ -1,5 +1,5 @@
 ; A159225: 1/4 the number of n X n arrays of squares of integers with every 2X2 subblock summing to 25
-; Submitted by PDW
+; Submitted by Simon Strandgaard
 ; 7,24,53,135,265,621,1169,2649,4897,10929,20033,44385,81025,178881,325889,718209,1307137,2878209,5235713,11523585,20957185,46115841,83857409,184506369,335486977
 
 add $0,3
@@ -12,8 +12,8 @@ lpb $0
   mul $4,$2
   add $4,$1
   mod $2,2
-  add $3,$4
 lpe
+add $3,$4
 mov $0,$3
 div $0,4
 sub $0,1
