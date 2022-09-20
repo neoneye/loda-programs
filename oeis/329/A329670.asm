589bcdbd5659d8d120049272f3546aedc99e32d6
Simon Strandgaard <neoneye@gmail.com>
2022-09-20 09:19:40 +0200

    Updated programs

--- oeis/329/A329670.asm
+++ oeis/329/A329670.asm
@@ -1,7 +1,20 @@
 ; A329670: Number of excursions of length n with Motzkin-steps allowing only consecutive steps UH and HD.
+; Submitted by Simon Strandgaard
 ; 1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-mov $1,4294967343
-gcd $1,$0
-bin $1,$0
-mov $0,$1
+add $0,1
+lpb $0
+  dif $0,2
+  pow $0,2
+  sub $0,1
+  mov $2,1
+  lpb $2
+    sub $2,1
+    add $1,2
+  lpe
+  mov $3,$1
+lpe
+mov $0,$3
+sub $0,2
+div $0,2
+add $0,1
