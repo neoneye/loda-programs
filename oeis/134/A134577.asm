03a6853f6ca5f8e60dacca145ae02a80d2b9f043
Simon Strandgaard <neoneye@gmail.com>
2022-09-22 21:06:55 +0200

    Updated programs

--- oeis/134/A134577.asm
+++ oeis/134/A134577.asm
@@ -1,27 +1,8 @@
 ; A134577: A127170 * A127648.
-; Submitted by [AF] Kalianthys
+; Submitted by Simon Strandgaard
 ; 1,2,2,2,0,3,3,4,0,4,2,0,0,0,5,4,4,6,0,0,6,2,0,0,0,0,0,7,4,6,0,8,0,0,0,8,3,0,6,0,0,0,0,0,9,4,4,0,0,10,0,0,0,0,10
 
-mov $4,1
-lpb $0
-  add $2,1
-  sub $0,$2
-lpe
-add $2,1
-mul $4,$0
-mov $0,10
-lpb $0
-  sub $0,1
-  add $4,1
-  gcd $5,$4
-  mul $1,$2
-  gcd $1,$2
-  div $1,$4
-  mul $1,$4
-  div $1,$2
-  mul $1,$5
-  add $3,$1
-  add $4,$5
-  sub $4,1
-lpe
-mov $0,$3
+mov $1,$0
+seq $1,127170 ; Triangle read by rows: T(n,k) is the number of divisors of n that are divisible by k, with 1 <= k <= n.
+seq $0,2260 ; Triangle read by rows: T(n,k) = k for n >= 1, k = 1..n.
+mul $0,$1
