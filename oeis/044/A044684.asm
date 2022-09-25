ba91cdea5c9b62d1fe7909f7641f844f8e6a20c3
Simon Strandgaard <neoneye@gmail.com>
2022-09-25 21:04:52 +0200

    Updated programs

--- oeis/044/A044684.asm
+++ oeis/044/A044684.asm
@@ -1,28 +1,20 @@
 ; A044684: Numbers n such that string 5,7 occurs in the base 9 representation of n but not of n+1.
-; Submitted by PDW
+; Submitted by Simon Strandgaard
 ; 52,133,214,295,376,457,476,538,619,700,781,862,943,1024,1105,1186,1205,1267,1348,1429,1510,1591,1672,1753,1834,1915,1934,1996,2077,2158,2239,2320,2401,2482,2563,2644,2663,2725,2806
 
 mov $4,$0
-mov $6,2
-add $0,2
 mov $2,$0
+add $2,4
 lpb $2
-  add $3,$0
-  add $6,$2
-  mov $5,$3
-  lpb $5
-    mov $5,4
-    sub $6,3
-  lpe
-  mod $1,2
-  add $1,39
+  add $3,$2
+  mov $1,5
   mov $2,0
-  sub $6,6
-  lpb $6
-    add $1,19
-    add $2,$6
+  sub $3,9
+  lpb $3
+    mov $1,24
+    add $2,$3
+    mov $3,0
     sub $4,1
-    trn $6,$3
   lpe
   trn $2,1
 lpe
@@ -31,4 +23,4 @@ lpb $4
   add $1,81
 lpe
 mov $0,$1
-add $0,13
+add $0,47
