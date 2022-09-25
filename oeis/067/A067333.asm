81050fdb79f6c910b9c76433ebd3d6cf4f006124
Simon Strandgaard <neoneye@gmail.com>
2022-09-26 00:49:54 +0200

    Updated programs

--- oeis/067/A067333.asm
+++ oeis/067/A067333.asm
@@ -1,22 +1,20 @@
 ; A067333: Convolution of Fibonacci F(n+1), n>=0, with F(n+5), n>=0.
-; Submitted by Christian Krause
+; Submitted by Simon Strandgaard
 ; 5,13,31,65,130,250,469,863,1565,2805,4980,8772,15349,26705,46235,79705,136886,234302,399845,680515,1155385,1957293,3309096,5584200,9407525,15823765,26577559,44579633,74681770
 
+mov $3,1
+mov $4,2
+mov $5,3
 add $0,1
 lpb $0
   sub $0,1
-  add $2,$5
+  add $3,2
   add $1,$3
-  sub $1,$2
-  add $2,$3
-  add $2,3
-  add $4,2
-  sub $4,$5
-  add $4,$1
-  mov $5,$4
-  add $5,$2
-  mov $4,$2
+  add $1,$4
   mov $2,$3
   mov $3,$5
+  mul $4,-1
+  add $4,$1
+  add $5,$2
 lpe
-mov $0,$3
+mov $0,$1
--- oeis/182/A182139.asm
+++ oeis/182/A182139.asm
@@ -7,20 +7,20 @@ mov $2,2
 add $0,1
 lpb $0
   mov $3,$0
-  sub $3,2
   pow $3,3
+  sub $3,2
   lpb $3
     mov $4,$0
     mod $4,$2
     add $2,1
     sub $3,$4
   lpe
-  mov $5,1
+  mov $5,2
   lpb $0
     dif $0,$2
     mul $5,2
-    add $5,1
   lpe
+  sub $5,1
   mul $1,$5
 lpe
 mul $0,$1
