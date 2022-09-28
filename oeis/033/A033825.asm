2ba18f1d85c9a50fbdbeef3ee77a0d609036c461
Simon Strandgaard <neoneye@gmail.com>
2022-09-28 16:50:32 +0200

    Updated programs

--- oeis/033/A033825.asm
+++ oeis/033/A033825.asm
@@ -1,20 +1,20 @@
 ; A033825: Finite sequence associated with M24.
-; Submitted by Christian Krause
+; Submitted by Simon Strandgaard
 ; 2,4,4,4,6,6,8,8,10,12,16,24
 
-mov $1,$0
-add $1,29
-add $0,1
-seq $0,240926 ; a(n) = 2 + L(2*n) = 2 + A005248(n), n >= 0, with the Lucas numbers (A000032).
-div $0,$1
-mul $1,2
-add $0,$1
-div $0,$1
-sub $0,6
-lpb $1
-  sub $1,2
-  add $0,2
+sub $0,11
+pow $0,2
+mul $0,10
+mov $1,9194
+mov $2,$0
+add $2,$0
+mov $0,9194
+lpb $0
+  mov $0,$2
+  trn $0,1
+  sub $1,72
+  div $2,2
 lpe
-div $0,5
-sub $0,9
-mul $0,2
+div $1,36
+mov $0,$1
+sub $0,229
--- oeis/343/A343939.asm
+++ oeis/343/A343939.asm
@@ -4,6 +4,7 @@
 
 add $0,1
 mov $1,1
+mov $2,1
 mov $7,$0
 lpb $0
   add $2,1
@@ -14,10 +15,7 @@ lpb $0
     mod $4,$2
     cmp $4,0
     cmp $4,0
-    mov $5,$2
-    cmp $5,1
     add $2,1
-    max $4,$5
     sub $3,$4
   lpe
   lpb $0
