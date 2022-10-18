ca570e90d8ba3e474a242717ceaea6d4b3d8fb0a
Simon Strandgaard <neoneye@gmail.com>
2022-10-18 14:49:04 +0200

    Updated programs

--- oeis/236/A236185.asm
+++ oeis/236/A236185.asm
@@ -2,12 +2,12 @@
 ; Submitted by Simon Strandgaard
 ; 4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2,6,4,2,4,2,4,6,2
 
-sub $0,22
+add $0,3
 sub $1,$0
 mov $0,1603
 sub $0,$1
 mod $0,8
-seq $0,36577 ; Ternary Thue-Morse sequence: closed under a->abc, b->ac, c->b.
+seq $0,313717 ; Coordination sequence Gal.6.151.6 where G.u.t.v denotes the coordination sequence for a vertex of type v in tiling number t in the Galebach list of u-uniform tilings.
 mul $0,2
 add $0,8
 mod $0,3
