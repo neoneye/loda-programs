ada03141e8ccd573026a7ff1945ba5fff0d6507c
Simon Strandgaard <neoneye@gmail.com>
2022-10-18 18:37:29 +0200

    Updated programs

--- oeis/269/A269815.asm
+++ oeis/269/A269815.asm
@@ -1,19 +1,18 @@
 ; A269815: Number of active (ON,black) cells at stage 2^n-1 of the two-dimensional cellular automaton defined by "Rule 35", based on the 5-celled von Neumann neighborhood.
-; Submitted by Jamie Morken(w4)
+; Submitted by Simon Strandgaard
 ; 1,5,37,185,817,3425,14017,56705,228097,914945,3664897,14669825,58699777,234840065,939442177,3757932545
 
-mov $1,1
-lpb $0
-  sub $0,1
-  mov $2,$1
-  add $3,5
-  mov $1,$3
-  mul $3,2
-  sub $3,5
-  mul $4,3
-  add $4,$2
-  add $1,$4
-lpe
-mov $0,$4
-mul $0,4
+sub $0,1
+mov $1,2
+pow $1,$0
+mov $2,$1
+mov $0,$1
+mul $0,2
+mov $1,$0
+add $1,$2
+add $2,$1
+add $1,$2
+sub $0,2
+mul $0,$1
+add $0,$2
 add $0,1
