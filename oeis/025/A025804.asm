9eba2ce557715bfdef2faa87461fbc249d4ed82f
Simon Strandgaard <neoneye@gmail.com>
2022-10-19 21:51:35 +0200

    Updated programs

--- oeis/025/A025804.asm
+++ oeis/025/A025804.asm
@@ -2,17 +2,14 @@
 ; Submitted by Simon Strandgaard
 ; 1,0,1,0,2,0,2,0,3,1,3,1,4,2,4,2,5,3,6,3,7,4,8,4,9,5,10,6,11,7,12,8,13,9,14,10,16,11,17,12,19,13,20,14,22,16,23,17,25,19,26,20,28,22,30,23,32,25,34,26,36,28,38,30,40
 
-mov $1,1
-add $0,3
-lpb $0
-  mov $2,$0
-  seq $2,14682 ; The Collatz or 3x+1 function: a(n) = n/2 if n is even, otherwise (3n+1)/2.
-  trn $2,$0
-  div $2,2
-  sub $0,1
-  trn $0,10
-  add $0,2
-  add $1,$2
-lpe
-sub $1,1
-mov $0,$1
+mov $1,$0
+div $1,2
+gcd $0,2
+pow $0,3
+add $0,6
+div $0,2
+add $0,$1
+sub $0,1
+pow $0,2
+add $0,8
+div $0,36
