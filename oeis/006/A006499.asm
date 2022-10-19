4b8c586fcb87be2d8d80ce256c03d8dec30b19f0
Simon Strandgaard <neoneye@gmail.com>
2022-10-19 14:32:05 +0200

    Updated programs

--- oeis/006/A006499.asm
+++ oeis/006/A006499.asm
@@ -1,5 +1,5 @@
 ; A006499: Number of restricted circular combinations.
-; Submitted by Christian Krause
+; Submitted by Simon Strandgaard
 ; 1,3,9,12,16,28,49,77,121,198,324,522,841,1363,2209,3572,5776,9348,15129,24477,39601,64078,103684,167762,271441,439203,710649,1149852,1860496,3010348,4870849,7881197,12752041,20633238,33385284,54018522
 
 add $0,2
@@ -12,6 +12,8 @@ add $2,$1
 mod $2,2
 sub $2,8
 sub $2,$1
-seq $0,32 ; Lucas numbers beginning at 2: L(n) = L(n-1) + L(n-2), L(0) = 2, L(1) = 1.
-sub $0,10
+sub $0,1
+mul $0,2
+seq $0,90244 ; a(0) = 1; a(1) = 2; a(n) = { a(n-1) + a(n-2) for n even, a(n-1) - a(n-2) for n odd }.
 sub $0,$2
+sub $0,10
--- oeis/228/A228305.asm
+++ oeis/228/A228305.asm
@@ -4,10 +4,11 @@
 
 sub $0,1
 mov $1,$0
+div $1,2
+mov $2,2
+pow $2,$1
 mod $0,2
-lpb $1
-  sub $1,2
-  mul $0,2
-  add $0,4
-lpe
-add $0,4
+add $0,1
+mul $0,$2
+mul $2,3
+add $0,$2
