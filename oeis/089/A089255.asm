e0ea3f032673cd4a4c8b8b4fa77c9e8826f280c4
Simon Strandgaard <neoneye@gmail.com>
2022-10-20 19:40:51 +0200

    Updated programs

--- oeis/089/A089255.asm
+++ oeis/089/A089255.asm
@@ -1,17 +1,16 @@
 ; A089255: Odd numbers n such that 2*n-5 is a prime.
-; Submitted by Christian Krause
+; Submitted by Simon Strandgaard
 ; 5,9,11,17,21,23,29,33,39,47,51,53,57,59,71,77,81,89,93,99,101,117,119,123,131,137,141,143,149,159,161,171,177,179,189,197,201,203,207,213,219,227,231,233,257,263,273,281,287,291,299,303,309,311,323,329,333
 
+mov $1,$0
+mov $2,$0
+add $2,2
 add $0,1
-mov $3,$0
-pow $3,5
-lpb $3
-  add $2,4
-  mov $1,$2
-  seq $1,10051 ; Characteristic function of primes: 1 if n is prime, else 0.
-  sub $0,$1
-  sub $3,$0
-lpe
-mov $0,$2
+mul $0,2
+seq $1,111215 ; Numbers k such that 4k + 5 is prime.
+mul $1,4
+add $1,5
+div $0,$2
+add $0,$1
+add $0,4
 div $0,2
-add $0,5
