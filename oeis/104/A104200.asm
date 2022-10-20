009715657c0674a3ffd26ab6a76a842c1a82dd0b
Simon Strandgaard <neoneye@gmail.com>
2022-10-20 17:38:13 +0200

    Updated programs

--- oeis/104/A104200.asm
+++ oeis/104/A104200.asm
@@ -1,6 +1,7 @@
 ; A104200: Upper bound on a straddle prime pair.
-; Submitted by paulteo
+; Submitted by Simon Strandgaard
 ; 5,7,11,11,11,13,17,17,17,19,23,23,23,29,29,29,29,29,31,37,37,37,37,37,41,41,41,43,47,47,47,53,53,53,53,53,59,59,59,59,59,61,67,67,67,67,67,71,71,71,73,79,79,79,79,79,83,83,83,89,89,89,89,89,97,97,97,97,97,97
 
-seq $0,72668 ; Numbers one less than composite numbers.
+add $0,1
+seq $0,18252 ; The nonprime numbers: 1 together with the composite numbers, A002808.
 seq $0,159477 ; a(n) = smallest prime >= n, if 1 is counted as a prime.
