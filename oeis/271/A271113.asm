7982e5345625563da7c4dc731c67dccd9a00eaa1
Simon Strandgaard <neoneye@gmail.com>
2022-10-28 18:26:41 +0200

    Updated programs

---
+++ oeis/271/A271113.asm
@@ -0,0 +1,16 @@
+; A271113: Numbers not in the range of the sum of abundant divisors function.
+; Submitted by Simon Strandgaard
+; 1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,19,21,22,23,25,26,27,28,29,31,33,34,35,37,39,41,43,45,46,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125,127,129,131,133,135,137,139
+
+mov $2,$0
+pow $2,2
+lpb $2
+  mov $3,$1
+  seq $3,270660 ; Numbers in the range of the sum of abundant divisors function.
+  sub $3,1
+  add $0,1
+  add $1,1
+  add $2,$3
+  sub $2,$0
+lpe
+add $0,1
---
+++ oeis/283/A283550.asm
@@ -0,0 +1,16 @@
+; A283550: Numbers that are not the sum of abundant numbers (not necessarily distinct).
+; Submitted by Simon Strandgaard
+; 1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,19,21,22,23,25,26,27,28,29,31,33,34,35,37,39,41,43,45,46,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107
+
+mov $2,$0
+pow $2,2
+lpb $2
+  mov $3,$1
+  seq $3,270660 ; Numbers in the range of the sum of abundant divisors function.
+  sub $3,1
+  add $0,1
+  add $1,1
+  add $2,$3
+  sub $2,$0
+lpe
+add $0,1
