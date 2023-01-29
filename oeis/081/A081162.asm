906f9e2dee5b3ffbeb83ea80338a17de8ca77f52
Simon Strandgaard <neoneye@gmail.com>
2023-01-29 20:09:21 +0100

    Updated programs

---
+++ oeis/081/A081162.asm
@@ -0,0 +1,9 @@
+; A081162: Number of even cycles in range [A014137(n-1)..A014138(n-1)] of permutation A057505/A057506, with two fixed-points of A057163.
+; Submitted by Simon Strandgaard (M1)
+; 0,0,0,0,0,0,0,1,0,4,0,15,0,56,0,185,0,655,0
+
+mov $1,$0
+mod $1,2
+div $0,2
+mul $0,$1
+seq $0,81163 ; Number of even cycles in range [A014137(2n)..A014138(2n)] of permutation A057505/A057506, with two fixed-points of A057163.
