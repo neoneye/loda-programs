6329bbc45646514d0889befde7c2b1b11a4ab13d
Simon Strandgaard <neoneye@gmail.com>
2023-04-03 17:07:57 +0200

    Updated programs

---
+++ oeis/360/A360329.asm
@@ -0,0 +1,9 @@
+; A360329: a(n) is the largest divisor of n that has only prime factors that are not prime-indexed primes.
+; Submitted by Simon Strandgaard (M1)
+; 1,2,1,4,1,2,7,8,1,2,1,4,13,14,1,16,1,2,19,4,7,2,23,8,1,26,1,28,29,2,1,32,1,2,7,4,37,38,13,8,1,14,43,4,1,46,47,16,49,2,1,52,53,2,1,56,19,58,1,4,61,2,7,64,13,2,1,4,23,14,71,8,73,74,1,76,7
+
+mov $1,$0
+add $1,1
+seq $0,360325 ; a(n) is the largest divisor of n that has only prime-indexed prime factors.
+div $1,$0
+mov $0,$1
